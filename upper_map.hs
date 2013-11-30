4bd99869c61878764ac156e5748236dab85d306c
Aaron Cronin <cronin@include.cat>
2013-11-30 20:59:34 +0000

    No more hashing, it's way too slow

--- upper_map.hs
+++ upper_map.hs
@@ -1,8 +1,7 @@
 import Data.Char (toUpper)
 import Data.List (lines, unlines)
-import Data.Hash.MD5
 
 main = interact $ unlines . map (\l -> keyF l ++ "\t" ++ valueF l) . lines
   where
-    keyF   = md5s . Str . valueF
+    keyF   = filter (/= '\t') . valueF
     valueF = map toUpper
