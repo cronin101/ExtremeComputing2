a0cb24619bfe691bc910a2f7b1aa5e606e159463
Aaron Cronin <cronin@include.cat>
2013-11-30 15:44:36 +0000

    Newline

--- upper_reducer.hs
+++ upper_reducer.hs
@@ -2,7 +2,6 @@ import Data.List (lines, unlines)
 import Data.List.Split (splitOn)
 import Data.String.Utils (join)
 
-
 main = interact $ unlines . map dropKey . lines
   where
     dropKey = join "\t" . tail . splitOn "\t"
