9abe5efd459c34be3c0eef32d5b13501ceea6b8e
Aaron Cronin <cronin@include.cat>
2013-11-30 15:42:43 +0000

    Removed unneeded imports

--- upper_map.hs
+++ upper_map.hs
@@ -1,8 +1,6 @@
-import System.IO (getContents)
 import Data.Char (toUpper)
 import Data.List (lines, unlines)
 import Data.Hash.MD5
-import Control.Monad
 
 main = interact $ unlines . map (\l -> keyF l ++ "\t" ++ valueF l) . lines
   where
