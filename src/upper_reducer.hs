5dade81c8df702e71ca961590d91f9a065900944
Aaron Cronin <acronin101@gmail.com>
2013-12-02 13:53:01 +0000

    No reducer needed

--- src/upper_reducer.hs
+++
@@ -1,7 +0,0 @@
-import Data.List (lines, unlines)
-import Data.List.Split (splitOn)
-import Data.String.Utils (join)
-
-main = interact $ unlines . map dropKey . lines
-  where
-    dropKey = join "\t" . tail . splitOn "\t"
